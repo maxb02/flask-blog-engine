e1c670d69319f1f6949a756a4709ef7bee201982
bondar_m <maxbondar02@hmail.com>
2019-07-23 17:34:56 +0300

    add admin page

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
 from config import Configuration
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 
+
+#App
 app = Flask(__name__)
 app.config.from_object(Configuration)
+
+#DB config
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+#Admin
+from models import Post, Tag
+admin = Admin(app)
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
--- config.py
+++ config.py
@@ -2,3 +2,4 @@ class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://flask_blog:flask_blog@localhost/flask_blog_db'
+    SECRET_KEY = 'To#^WgH0y%LZWQ1zh0wlVDdY'
